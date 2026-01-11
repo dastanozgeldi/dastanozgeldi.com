a944e13134254aca1ae4fb35f4da7f1e69103a53
dastanozgeldi <ozgdastan@gmail.com>
2026-01-12 01:00:16 +0500

    simplify

--- app/page.tsx
+++ app/page.tsx
@@ -2,30 +2,32 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="my-12 flex flex-col space-y-2.5 items-center justify-center">
-      <p>hi, i'm dastan</p>
-      <ul className="list-disc">
-        <li>i&apos;m an 18 y.o. builder from Almaty, Kazakhstan.</li>
-        <li>i've been programming since 13.</li>
-        <li>
-          i'm a first-year undergrad student in Tokyo and a software engineer at{" "}
-          <a href="https://rette.ai" className="underline">
-            rette.ai
-          </a>
-          .
-        </li>
-        <li>
-          you might wanna look at what i've{" "}
-          <Link href="/projects" className="underline">
-            built
-          </Link>{" "}
-          or{" "}
-          <Link href="/blog" className="underline">
-            written
-          </Link>{" "}
-          so far.
-        </li>
-      </ul>
+    <div className="mb-6 flex items-center justify-center">
+      <div className="flex flex-col space-y-2.5">
+        <p>hi, i'm dastan</p>
+        <ul className="list-disc list-inside">
+          <li>i&apos;m an 18 y.o. builder from Almaty, Kazakhstan.</li>
+          <li>i've been programming since 13.</li>
+          <li>
+            i'm a software engineer at{" "}
+            <a href="https://rette.ai" className="underline">
+              rette.ai
+            </a>
+            .
+          </li>
+          <li>
+            you might wanna look at what i've{" "}
+            <Link href="/projects" className="underline">
+              built
+            </Link>{" "}
+            or{" "}
+            <Link href="/blog" className="underline">
+              written
+            </Link>{" "}
+            so far.
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
